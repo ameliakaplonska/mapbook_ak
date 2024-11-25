4efd450e3e295a96746dc20458c9292e68a0902e
ameliakaplonska <amelia.kaplonska@student.wat.edu.pl>
2024-11-25 14:33:03 +0100

    feat: add read functionalities

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-users: list = [
-    {'name': 'Amelia', 'posts': 1, 'city': 'Warszawa'},
-    {'name': 'Dominik', 'posts': 3, 'city': 'Poznań'},
-    {'name': 'Szymon z wąsem', 'posts': 5, 'city': 'Toruń'},
-    {'name': 'Szymon', 'posts': 7, 'city': 'Łódź'},
-    {'name': 'Patryk', 'posts': 9, 'city': 'Kielce'},
-    {'name': 'Żerom', 'posts': 11, 'city': 'Katowice'},
-    {'name': 'Julia', 'posts': 15, 'city': 'Wrocław'},
-    {'name': 'Ola', 'posts': 11, 'city': 'Radom'},
-    {'name': 'Patrycja', 'posts': 20, 'city': 'Lublin'},
-    {'name': 'Patrycja', 'posts': 5, 'city': 'Mińsk Mazowiecki'},
+from mapbook.users import users
+from mapbook.crud import hello, read_users
 
-]
+def main():
+    hello(users[0]['name'])
+    read_users(users)
 
-print(f'Witaj {users[0]['name']}!!')
-for user in users[1:]:
-    print(f'twój znajomy {user['name']} z miasta {user['city']}, opublikował {user['posts']} postów')
-# for idx, _ in enumerate(users[1:]):
-#     print(f'twój znajomy {users[idx]}, opublikował {postow[idx]} postów')
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ mapbook/users.py
@@ -0,0 +1,12 @@
+users: list = [
+    {'name': 'Amelia', 'posts': 1, 'city': 'Warszawa'},
+    {'name': 'Dominik', 'posts': 3, 'city': 'Poznań'},
+    {'name': 'Szymon z wąsem', 'posts': 5, 'city': 'Toruń'},
+    {'name': 'Szymon', 'posts': 7, 'city': 'Łódź'},
+    {'name': 'Patryk', 'posts': 9, 'city': 'Kielce'},
+    {'name': 'Żerom', 'posts': 11, 'city': 'Katowice'},
+    {'name': 'Julia', 'posts': 15, 'city': 'Wrocław'},
+    {'name': 'Ola', 'posts': 11, 'city': 'Radom'},
+    {'name': 'Patrycja', 'posts': 20, 'city': 'Lublin'},
+    {'name': 'Patrycja', 'posts': 5, 'city': 'Mińsk Mazowiecki'},
+]
